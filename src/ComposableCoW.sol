fbb283966798fd93c9744e13d3db49764bcc167e
mfw78 <mfw78@protonmail.com>
2023-03-13 09:10:46 +0000

    feat: initial composable cow

---
+++ src/ComposableCoW.sol
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.0 <0.9.0;
+
+import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import {Safe} from "safe/Safe.sol";
+import {GPv2Order} from "cowprotocol/libraries/GPv2Order.sol";
+
+import {ISafeSignatureVerifier, ERC1271} from "handler/SignatureVerifierMuxer.sol";
+
+import {ConditionalOrder} from "./interfaces/ConditionalOrder.sol";
+
+contract ComposableCow is ISafeSignatureVerifier {
+    // A mapping of user's merkle roots
+    mapping (Safe => bytes32) public roots;
+    bytes32 public immutable settlementDomainSeparator;
+
+    // An enum representing different ways to store proofs
+    enum ProofStorage {
+        None,
+        Emit,
+        Swarm,
+        IPFS
+    }
+
+    // A struct representing where to find the proofs
+    struct Proof {
+        ProofStorage storageType;
+        bytes payload;
+    }
+
+    // An event emitted when a user sets their merkle root
+    event RootSet(address indexed usr, bytes32 root, Proof proof);
+
+    constructor(bytes32 _settlementDomainSeparator) {
+        settlementDomainSeparator = _settlementDomainSeparator;
+    }
+
+    /// @notice Set the merkle root of the user's conditional orders
+    /// @param root The merkle root of the user's conditional orders
+    /// @param proof Where to find the proofs
+    function setRoot(bytes32 root, Proof calldata proof) external {
+        roots[Safe(payable(msg.sender))] = root;
+        emit RootSet(msg.sender, root, proof);
+    }
+
+    function isValidSafeSignature(
+        Safe safe,
+        address sender,
+        bytes32 hash,
+        bytes calldata signature
+    ) external view override returns (bytes4 magic) {
+        // The signature is an abi.encode(bytes32[] proof, address orderHandler, bytes orderData, bytes32 salt)
+        (bytes32[] memory proof, ConditionalOrder handler, bytes memory data, bytes32 salt) = abi.decode(
+            signature,
+            (bytes32[], ConditionalOrder, bytes, bytes32)
+        );
+
+        // Computing proof using leaf double hashing
+        // https://flawed.net.nz/2018/02/21/attacking-merkle-trees-with-a-second-preimage-attack/
+        bytes32 root = roots[safe];
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(handler, data, salt))));
+
+        // Verify the proof
+        require(
+            MerkleProof.verify(proof, root, leaf),
+            "ComposableCow: invalid proof"
+        );
+
+        // The order is valid, so we can get the tradeable order
+        GPv2Order.Data memory order = handler.getTradeableOrder(address(safe), sender, data);
+
+        if (hash == GPv2Order.hash(order, settlementDomainSeparator)) {
+            magic = ERC1271.isValidSignature.selector;
+        }
+    }
+}
--- src/interfaces/ConditionalOrder.sol
+++ src/interfaces/ConditionalOrder.sol
@@ -47,22 +47,10 @@ interface ConditionalOrder {
     /// @dev This error is returned by the `getTradeableOrder` function if the order is cancelled.
     error OrderCancelled();
 
-    /// @dev This event is emitted by the Safe when a conditional order is created.
-    ///      The `address` of the Safe that implements the `getTradeableOrder` function.
-    ///      The `bytes` parameter is the abi-encoded order that is passed to the CoW Protocol API
-    ///      the signature bytes.
-    event ConditionalOrderCreated(address indexed, bytes);
-
-    /// @dev Using the `payload` supplied, create a conditional order that can be posted to the CoW Protocol API. The
-    ///      payload may be mutated by the function to create the order, which is then emitted as a
-    ///      `ConditionalOrderCreated` event.
-    /// @param payload The implementation-specific payload used to create the order
-    function dispatch(bytes calldata payload) external;
-
     /// @dev Get a tradeable order that can be posted to the CoW Protocol API and would pass signature validation.
     ///      Reverts if the order condition is not met.
     /// @param payload The implementation-specific payload used to create the order, as emitted by the
     ///        ConditionalOrderCreated event
     /// @return order to be posted to the CoW Protocol API
-    function getTradeableOrder(bytes calldata payload) external view returns (GPv2Order.Data memory);
+    function getTradeableOrder(address owner, address sender, bytes calldata payload) external view returns (GPv2Order.Data memory);
 }
