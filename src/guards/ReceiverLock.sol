0b50a42c11999012bc9621e0a2a150dfe227eb0d
mfw78 <mfw78@protonmail.com>
2023-04-19 07:01:27 +0000

    fix: fmt

--- src/guards/ReceiverLock.sol
+++ src/guards/ReceiverLock.sol
@@ -7,14 +7,9 @@ contract ReceiverLock is ISwapGuard {
     /**
      * Only allow orders with a receiver of 0x0 (ie. self)
      * @param order The order being verified
-     * @param payload Any additional data needed to verify the order (unused) 
+     * @param payload Any additional data needed to verify the order (unused)
      */
-    function verify(GPv2Order.Data calldata order, bytes calldata payload)
-        external
-        pure
-        override
-        returns (bool)
-    {
+    function verify(GPv2Order.Data calldata order, bytes calldata payload) external pure override returns (bool) {
         payload;
         return order.receiver == address(0);
     }
