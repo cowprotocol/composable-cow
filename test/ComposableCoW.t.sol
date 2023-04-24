c8e3c9ab65a409e69ac6665190acc65f6d2fc8cd
mfw78 <mfw78@protonmail.com>
2023-04-24 11:54:02 +0000

    tests: composable cow coverage

--- test/ComposableCoW.t.sol
+++ test/ComposableCoW.t.sol
@@ -15,6 +15,7 @@ import {TestAccount, TestAccountLib} from "./libraries/TestAccountLib.t.sol";
 import {SafeLib} from "./libraries/SafeLib.t.sol";
 
 import {TWAP, TWAPOrder} from "../src/types/twap/TWAP.sol";
+import {IConditionalOrder} from "../src/interfaces/IConditionalOrder.sol";
 import "../src/ComposableCoW.sol";
 
 contract ComposableCoWTest is Base, Merkle {
@@ -25,14 +26,14 @@ contract ComposableCoWTest is Base, Merkle {
     ComposableCoW composableCow;
     TWAP twap;
 
-    mapping(bytes32 => ComposableCoW.ConditionalOrderParams) public leaves;
+    mapping(bytes32 => IConditionalOrder.ConditionalOrderParams) public leaves;
 
     function setUp() public virtual override(Base) {
         // setup Base
         super.setUp();
 
         // deploy composable cow
-        composableCow = new ComposableCoW();
+        composableCow = new ComposableCoW(settlement.domainSeparator());
 
         // set safe1 to have the ComposableCoW `ISafeSignatureVerifier` custom verifier
         // we will set the domainSeparator to settlement.domainSeparator()
@@ -70,10 +71,13 @@ contract ComposableCoWTest is Base, Merkle {
         });
 
         // 2. Create four conditional orders as leaves of the ComposableCoW
-        ComposableCoW.ConditionalOrderParams[] memory _leaves = new ComposableCoW.ConditionalOrderParams[](4);
+        IConditionalOrder.ConditionalOrderParams[] memory _leaves = new IConditionalOrder.ConditionalOrderParams[](4);
         for (uint256 i = 0; i < _leaves.length; i++) {
-            _leaves[i] =
-                ComposableCoW.ConditionalOrderParams({handler: twap, salt: bytes32(i), data: abi.encode(twapData)});
+            _leaves[i] = IConditionalOrder.ConditionalOrderParams({
+                handler: twap,
+                salt: bytes32(i),
+                staticInput: abi.encode(twapData)
+            });
 
             leaves[hashLeaf(_leaves[i])] = _leaves[i];
         }
@@ -94,61 +98,36 @@ contract ComposableCoWTest is Base, Merkle {
         bytes32[] memory proof = getProof(sortedHashes, 0);
 
         // 7. Get the leaf that was used to create the proof
-        ComposableCoW.ConditionalOrderParams memory leaf = leaves[sortedHashes[0]];
+        IConditionalOrder.ConditionalOrderParams memory leaf = leaves[sortedHashes[0]];
 
         // 8. Set the Merkle root
         safe1.execute(
             address(composableCow),
             0,
-            abi.encodeWithSelector(
-                composableCow.setRoot.selector,
-                root,
-                ComposableCoW.Proof({storageType: ComposableCoW.ProofStorage.None, payload: ""})
-            ),
+            abi.encodeWithSelector(composableCow.setRoot.selector, root, ComposableCoW.Proof({location: 0, data: ""})),
             Enum.Operation.Call,
             signers()
         );
 
-        // 9. The GPvOrder.Data for the TWAP segment
-        (GPv2Order.Data memory order,) = twap.getTradeableOrder(address(safe1), address(0), leaf.data);
-
-        bytes memory encodeData = abi.encode(order);
-
-        // bytes32 domainSeparator, bytes32 typeHash, bytes32 encodeData)
-        // As this is for a Safe signer, we need to abi.encode the order
-        // into safeSignature(bytes32,bytes32,bytes32,bytes)
-        bytes memory signature = abi.encodeWithSignature(
-            "safeSignature(bytes32,bytes32,bytes,bytes)",
-            settlement.domainSeparator(),
-            GPv2Order.TYPE_HASH,
-            encodeData,
-            abi.encode(proof, leaf)
+        // 9. Get the order and signature
+        (GPv2Order.Data memory order, bytes memory signature) = composableCow.getTradeableOrderWithSignature(
+            address(safe1), leaf.handler, leaf.salt, leaf.staticInput, bytes(""), proof
         );
 
         // 10. Set the ERC20 allowance for the TWAP
         safe1.execute(
             address(token0),
             0,
-            abi.encodeWithSelector(
-                IERC20.approve.selector,
-                address(relayer),
-                twapData.n * twapData.partSellAmount
-            ),
+            abi.encodeWithSelector(IERC20.approve.selector, address(relayer), twapData.n * twapData.partSellAmount),
             Enum.Operation.Call,
             signers()
         );
 
         // 11. Execute the order
         settle(address(safe1), bob, order, signature);
-
-        // 9. Construct the signature payload
-        // bytes memory data =
-        //     abi.encodeCall(ERC1271.isValidSignature, (GPv2Order.hash(order, settlement.domainSeparator()), signature));
-
-        // (bool success, bytes memory result) = address(safe1).staticcall(data);
     }
 
-    function hashLeaf(ComposableCoW.ConditionalOrderParams memory leaf) public pure returns (bytes32) {
+    function hashLeaf(IConditionalOrder.ConditionalOrderParams memory leaf) public pure returns (bytes32) {
         return keccak256(bytes.concat(keccak256(abi.encode(leaf))));
     }
 
