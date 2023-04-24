4d455374b70de4f0515c5bd3b49554d6edba79fd
mfw78 <mfw78@protonmail.com>
2023-04-24 11:50:30 +0000

    feat: receiver lock swapguard

---
+++ src/guards/BaseGuard.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.0 <0.9.0;
+
+import "../interfaces/ISwapGuard.sol";
+
+/**
+ * @title An abstract base contract for Swap Guards to inherit from
+ * @author mfw78 <mfw78@rndlabs.xyz>
+ */
+abstract contract BaseGuard is ISwapGuard {
+    /**
+     * @inheritdoc IERC165
+     */
+    function supportsInterface(bytes4 interfaceId) external view virtual override returns (bool) {
+        return interfaceId == type(ISwapGuard).interfaceId || interfaceId == type(IERC165).interfaceId;
+    }
+}
--- src/guards/ReceiverLock.sol
+++ src/guards/ReceiverLock.sol
@@ -1,16 +1,23 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.0 <0.9.0;
 
-import "../interfaces/ISwapGuard.sol";
+import "./BaseGuard.sol";
 
-contract ReceiverLock is ISwapGuard {
+/**
+ * @title A Swap Guard that only allows orders with a receiver of 0x0 (ie. self)
+ * @author mfw78 <mfw78@rndlabs.xyz>
+ */
+contract ReceiverLock is BaseGuard {
     /**
      * Only allow orders with a receiver of 0x0 (ie. self)
      * @param order The order being verified
-     * @param payload Any additional data needed to verify the order (unused)
      */
-    function verify(GPv2Order.Data calldata order, bytes calldata payload) external pure override returns (bool) {
-        payload;
+    function verify(GPv2Order.Data calldata order, IConditionalOrder.ConditionalOrderParams calldata, bytes calldata)
+        external
+        pure
+        override
+        returns (bool)
+    {
         return order.receiver == address(0);
     }
 }
