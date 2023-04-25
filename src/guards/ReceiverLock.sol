f0def6947c387183ba1650953d464813f96885c4
mfw78 <mfw78@protonmail.com>
2023-04-25 12:41:43 +0000

    fix: naming collision

--- src/guards/ReceiverLock.sol
+++ src/guards/ReceiverLock.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.0 <0.9.0;
 
-import "./BaseGuard.sol";
+import "./BaseSwapGuard.sol";
 
 /**
  * @title A Swap Guard that only allows orders with a receiver of 0x0 (ie. self)
  * @author mfw78 <mfw78@rndlabs.xyz>
  */
-contract ReceiverLock is BaseGuard {
+contract ReceiverLock is BaseSwapGuard {
     /**
      * Only allow orders with a receiver of 0x0 (ie. self)
      * @param order The order being verified
