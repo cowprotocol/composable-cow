b3c55dae9f1414c5dfd9ef7f07212af708dec494
mfw78 <mfw78@protonmail.com>
2023-04-30 06:52:27 +0000

    chore: rationalise imports

--- src/types/twap/TWAP.sol
+++ src/types/twap/TWAP.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.0 <0.9.0;
 
-import {GPv2Order} from "cowprotocol/libraries/GPv2Order.sol";
-
 import "../../BaseConditionalOrder.sol";
 import {TWAPOrder} from "./libraries/TWAPOrder.sol";
 
