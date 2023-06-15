22946ee60f6143af72801cd801b0ef873c2ceb4f
mfw78 <mfw78@protonmail.com>
2023-06-15 09:23:48 +0000

    chore: value factory deploy script

---
+++ script/deploy_ValueFactories.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.0 <0.9.0;
+
+import "forge-std/Script.sol";
+
+import "../src/ComposableCoW.sol";
+
+import {CurrentBlockTimestampFactory} from "../src/value_factories/CurrentBlockTimestampFactory.sol";
+
+contract DeployValueFactories is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        new CurrentBlockTimestampFactory();
+
+        vm.stopBroadcast();
+    }
+}
