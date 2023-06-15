2fe50de75f01b9739c5812ce982b7368ebb75cad
mfw78 <mfw78@protonmail.com>
2023-06-15 09:25:03 +0000

    chore: production stack deploy

---
+++ script/deploy_ProdStack.s.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.0 <0.9.0;
+
+import "forge-std/Script.sol";
+
+// ExtensibleFallbackHandler
+import {ExtensibleFallbackHandler} from "../lib/safe/contracts/handler/ExtensibleFallbackHandler.sol";
+
+// ComposableCoW
+import {ComposableCoW} from "../src/ComposableCoW.sol";
+
+// Order types
+import {TWAP} from "../src/types/twap/TWAP.sol";
+
+// Value factories
+import {CurrentBlockTimestampFactory} from "../src/value_factories/CurrentBlockTimestampFactory.sol";
+
+contract DeployProdStack is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        address settlement = vm.envAddress("SETTLEMENT");
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        // Deploy ExtensibleFallbackHandler
+        new ExtensibleFallbackHandler();
+
+        // Deploy ComposableCoW
+        ComposableCoW composableCow = new ComposableCoW(settlement);
+
+        // Deploy order types
+        new TWAP(composableCow);
+
+        // Deploy value factories
+        new CurrentBlockTimestampFactory();
+    }
+}
