006b08c2d5700dee1dbb28f318c9755641c5b57c
Pedro Yves Fracari <yves.fracari@gmail.com>
2024-08-05 10:56:33 -0300

    fix test function name typo

--- test/ComposableCoW.stoploss.t.sol
+++ test/ComposableCoW.stoploss.t.sol
@@ -287,7 +287,7 @@ contract ComposableCoWStopLossTest is BaseComposableCoWTest {
         stopLoss.getTradeableOrder(safe, address(0), bytes32(0), abi.encode(data), bytes(""));
     }
 
-    function test_OracleRevertOnExperiedOrder_fuzz(
+    function test_OracleRevertOnExpiriedOrder_fuzz(
         uint32 currentTime,
         uint32 validTo
     ) public {
