2fdefe488fb54184e1d8860eda5da04e9991c1ee
mfw78 <mfw78@nullislabs.io>
2026-02-06 17:42:34 +0000

    feat: extend CoWSettlement interface with filledAmount
    Add filledAmount(bytes calldata orderUid) for querying order fill status.

--- src/vendored/CoWSettlement.sol
+++ src/vendored/CoWSettlement.sol
@@ -3,4 +3,5 @@ pragma solidity >=0.8.0 <0.9.0;
 
 interface CoWSettlement {
     function domainSeparator() external view returns (bytes32);
+    function filledAmount(bytes calldata orderUid) external view returns (uint256);
 }
