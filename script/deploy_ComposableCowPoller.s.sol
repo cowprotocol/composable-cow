96f3674d9e6ff7d4b42a77d0eb394acfc4f08d4c
igorroncevic <57319163+igorroncevic@users.noreply.github.com>
2026-07-16 17:14:31 +0200

    chore: nits

--- script/deploy_ComposableCowPoller.s.sol
+++ script/deploy_ComposableCowPoller.s.sol
@@ -7,14 +7,15 @@ import {ComposableCoW} from "../src/ComposableCoW.sol";
 import {ComposableCowPoller} from "../src/types/ComposableCowPoller.sol";
 
 contract DeployComposableCowPoller is Script {
+    error InvalidComposableCow();
+
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         address composableCow = vm.envAddress("COMPOSABLE_COW");
+        if (composableCow.code.length == 0) revert InvalidComposableCow();
 
         vm.startBroadcast(deployerPrivateKey);
-
         ComposableCowPoller poller = new ComposableCowPoller(ComposableCoW(composableCow));
-
         vm.stopBroadcast();
 
         console.log("ComposableCowPoller address");
