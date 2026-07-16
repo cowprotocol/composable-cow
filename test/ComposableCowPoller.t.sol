be3ac1946f33966feb1dcf525421e385f23ccf65
igorroncevic <57319163+igorroncevic@users.noreply.github.com>
2026-07-16 16:32:07 +0200

    chore: use absolute imports

--- test/ComposableCowPoller.t.sol
+++ test/ComposableCowPoller.t.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.0 <0.9.0;
 
-import {BaseComposableCoWTest} from "./ComposableCoW.base.t.sol";
+import {BaseComposableCoWTest} from "test/ComposableCoW.base.t.sol";
 
-import {ComposableCowPoller} from "../src/types/ComposableCowPoller.sol";
+import {ComposableCowPoller} from "src/types/ComposableCowPoller.sol";
 
 /// @title ComposableCowPoller unit tests
 /// @notice Base scaffolding for the poller. Feature-specific tests (register / revoke / topUp) are
